44db726d8f184d4f258952a653ea9585efe74191
Noah Meyerhans <nmeyerhans@linux.microsoft.com>
2026-01-30 10:19:59 -0500

    ppc64el: generate early grub config

--- cloud/system-boot/mkosi.conf.d/ppc64el/mkosi.finalize.d/20-grub-ppc64le.sh
+++ cloud/system-boot/mkosi.conf.d/ppc64el/mkosi.finalize.d/20-grub-ppc64le.sh
@@ -7,19 +7,27 @@ set -euE
 
 . "${SRCDIR}/partitions"
 
-test -n "$PARTUUID_ROOT"
+test -n "$FSUUID_ROOT"
 
 mkosi-chroot cp -r /usr/lib/grub/powerpc-ieee1275 \
              /boot/grub
 mkosi-chroot mkdir -p /boot/grub/fonts/
 mkosi-chroot cp /usr/share/grub/unicode.pf2 /boot/grub/fonts/
 mkosi-chroot grub-editenv /boot/grub/grubenv create
+
+cat <<EOF > "${BUILDROOT}/boot/grub/powerpc-ieee1275/load.cfg"
+search.fs_uuid ${FSUUID_ROOT} root
+set prefix=(\$root)'/boot/grub'
+configfile \$prefix/grub.cfg
+EOF
+
 mkosi-chroot grub-mkimage \
              --verbose \
              --directory '/usr/lib/grub/powerpc-ieee1275' \
-             --prefix '(,gpt2)/boot/grub' \
+             --prefix '(,gpt1)/boot/grub' \
              --output '/boot/grub/powerpc-ieee1275/core.elf' \
              --format 'powerpc-ieee1275' \
+             --config '/boot/grub/powerpc-ieee1275/load.cfg' \
              --compression 'auto' 'gzio' 'boot' 'bufio' 'cat' 'cmp' 'configfile' 'crypto' 'datetime' 'disk' 'div' 'echo' 'ext2' 'fat' 'fshelp' 'gcry_crc' 'gettext' 'hello' 'help' 'linux' 'loadenv' 'ls' 'minicmd' 'normal' 'part_gpt' 'priority_queue' 'read' 'reboot' 'search' 'search_fs_file' 'sleep' 'terminal' 'test' 'tr' 'true' 'gfxmenu' 'search_fs_uuid' 'cpio'
 
 test -f "${BUILDROOT}/boot/grub/powerpc-ieee1275/core.elf"
