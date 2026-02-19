b2a76954bde5824138dde0122e04b70f9962986a
Noah Meyerhans <nmeyerhans@linux.microsoft.com>
2026-02-19 09:20:30 -0500

    generate more reproducible UUIDs
    generates UUIDs for the rootfs directory hash, the gpt partition table, the
    powerpc PReP partition, and the BIOS boot partition. Also generates a
    reproducible serial number for the ESP FAT filesystem as appropriate.

--- cloud/mkosi.conf.d/system-boot/mkosi.sync.d/10-partitions.sh
+++ cloud/mkosi.conf.d/system-boot/mkosi.sync.d/10-partitions.sh
@@ -6,13 +6,28 @@
 # Note that not all images will use all UUIDs generated here.
 outfile="${SRCDIR}/partitions"
 
-echo PARTUUID_ROOT=$(uuid -v 5 "$SEED_UUID" PARTUUID_ROOT) > "${outfile}"
-echo FSUUID_ROOT=$(uuid -v 5 "$SEED_UUID" FSUUID_ROOT) >> "${outfile}"
-echo PARTUUID_ESP=$(uuid -v 5 "$SEED_UUID" PARTUUID_ESP) >> "${outfile}"
+exec > "$outfile"
+echo PARTUUID_ROOT=$(uuid -v 5 "$SEED_UUID" PARTUUID_ROOT)
+echo FSUUID_ROOT=$(uuid -v 5 "$SEED_UUID" FSUUID_ROOT)
+echo DIRHASH_ROOT=$(uuid -v 5 "$SEED_UUID" DIRHASH_ROOT)
+echo GPT_TABLE_UUID=$(uuid -v 5 "$SEED_UUID" GPT_TABLE_UUID)
+
+case "$DISTRIBUTION_ARCHITECTURE" in
+    ppc64el)
+        echo PARTUUID_PREP=$(uuid -v 5 "$SEED_UUID" PARTUUID_PREP)
+        ;;
+    arm64|amd64)
+        echo PARTUUID_ESP=$(uuid -v 5 "$SEED_UUID" PARTUUID_ESP)
+        echo -n "ESP_VFAT_SERIAL="
+        python3 -c "import random; random.seed(\"$SEED_UUID\"); print(random.randint(0, 99999999))"
+        # only actually meaningful on amd64...
+        echo PARTUUID_BIOS=$(uuid -v 5 "$SEED_UUID" PARTUUID_BIOS)
+        ;;
+esac
 
 . "${outfile}"
 
 test -n "$PARTUUID_ROOT"
 
-echo "Generated build-specific UUIDs:"
-cat "${outfile}"
+echo "Generated build-specific UUIDs:" >&2
+cat "${outfile}" >&2
