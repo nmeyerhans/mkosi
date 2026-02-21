ef8134b330d7f7931ec2d303a2b6b02fb5d93f05
Noah Meyerhans <nmeyerhans@linux.microsoft.com>
2026-02-20 20:26:28 -0500

    port additional image prep/cleanup from FAI

--- cloud/mkosi.postinst.d/90-cleanup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# mkosi leaves some things in the wrong place.
-rmdir "${BUILDROOT}/efi" 2> /dev/null
-rm -f "${BUILDROOT}"/vmlinu[zx]* "${BUILDROOT}"/initrd.* "${BUILDROOT}"/init
-
-exit 0
